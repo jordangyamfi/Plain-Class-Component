b141b92577d309f11cb4088ee780b6e1c56b2b13
jordangyamfi <jordan.gyamfi1@gmail.com>
2021-01-28 15:22:19 +0000

    finish

---
+++ arrow-function/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./components/Message"
+
+class WelcomeComponent extends Component  {
+  
+
+  render () {
+  return  <h1>Codetrain is awesome, react works.</h1>
+  }
+}
+
+export default WelcomeComponent;
---
+++ arrow-function/src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Message({props}) {
+  return (
+    <div> 
+    <h1>Codetrain is awesome, react works.</h1>
+    </div>
+  ); 
+}
+
+export default Message;
